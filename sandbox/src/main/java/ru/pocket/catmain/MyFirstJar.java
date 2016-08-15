f44fc16831983c5daef8ccdb176a865c087b4f53
Anna <verdyanna@gmail.com>
2016-08-15 13:00:16 +0300

    Создана функция hello

--- sandbox/src/main/java/ru/pocket/catmain/MyFirstJar.java
+++ sandbox/src/main/java/ru/pocket/catmain/MyFirstJar.java
@@ -2,6 +2,10 @@ package ru.pocket.catmain;
 
 public class MyFirstJar {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        hello();
+
+    }
+    public  static  void hello(){
+      System.out.println("Hello, wold!");
     }
 }
