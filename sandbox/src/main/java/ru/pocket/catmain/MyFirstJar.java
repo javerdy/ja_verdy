4483a177264a3eecd8a9c3fe5c6352656717a3f8
Anna <verdyanna@gmail.com>
2016-08-16 11:23:11 +0300

    Созданы функции - площадь квадрата и прямоугольника

--- sandbox/src/main/java/ru/pocket/catmain/MyFirstJar.java
+++ sandbox/src/main/java/ru/pocket/catmain/MyFirstJar.java
@@ -1,11 +1,32 @@
 package ru.pocket.catmain;
 
 public class MyFirstJar {
-    public static void main(String[] args) {
-        hello();
+  public static void main(String[] args) {
+        hello("world");
+    hello("user");
+    hello("Anna");
 
-    }
-    public  static  void hello(){
-      System.out.println("Hello, wold!");
-    }
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной"+ len + "=" + area(len));
+
+    double a = 4;
+    double b = 8;
+    System.out.println("Площадь прямоугольника со сторонами"+ a +"и"+ b +"=" + area(a, b));
+
+  }
+
+  public static void hello(String someword) {
+
+    System.out.println("Hello," + someword + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+
+  }
+  public static double area(double a, double b){
+    return a * b;
+  }
+
+
 }
