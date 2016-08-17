082c6a561ec675c87165663e3b44130eb2a120f7
Anna <verdyanna@gmail.com>
2016-08-18 02:46:19 +0300

    Расстояние между точками с использованием конструктора.

--- sandbox/src/main/java/ru/pocket/catmain/Point.java
+++ sandbox/src/main/java/ru/pocket/catmain/Point.java
@@ -5,12 +5,25 @@ package ru.pocket.catmain;
  */
 public class Point {
 
-  public double x1;
-  public double y1;
-  public double x2;
-  public double y2;
+  public double x;
+  public double y;
 
-}
+
+  public Point(double x, double y) {
+
+    this.x = x;
+    this.y = y;
+
+  }
+  public double distance(Point p2) {
+
+    return Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
+
+  }
+
+  }
+
+
 
 
 
