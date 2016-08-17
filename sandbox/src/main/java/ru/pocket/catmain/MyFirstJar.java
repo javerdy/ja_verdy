fdba9455a19739ce7b1119eb9bbe5e4ad7b8dcf1
Anna <verdyanna@gmail.com>
2016-08-17 13:23:24 +0300

    Решение задачи"расстояние между точками на плоскости"  без конструктора

--- sandbox/src/main/java/ru/pocket/catmain/MyFirstJar.java
+++ sandbox/src/main/java/ru/pocket/catmain/MyFirstJar.java
@@ -12,6 +12,16 @@ public class MyFirstJar {
     Rectangle f = new Rectangle(4, 8);
     System.out.println("Площадь прямоугольника со сторонами"+ f.a +"и"+ f.b +"=" + f.area());
 
+    Point p1 = new Point();
+    p1.x1 = 7;
+    p1.y1 = 8;
+
+    Point p2 = new Point();
+
+    p2.x2 = 3;
+    p2.y2 = 2;
+
+    System.out.println("Расстояние между точками"+ "(" + p1.x1 +"," + p1.y1 + ")" + "и"+ "("+ p2.x2 +","+ p2.y2 +")" + "=" + distance(p1,p2));
   }
 
   public static void hello(String someword) {
@@ -19,4 +29,8 @@ public class MyFirstJar {
     System.out.println("Hello," + someword + "!");
   }
 
+  public static double distance (Point p1, Point p2){
+
+    return  Math.sqrt((p2.x2-p1.x1)*(p2.x2-p1.x1)+(p2.y2-p1.y1)*(p2.y2-p1.y1));
+}
 }
--- sandbox/src/main/java/ru/pocket/catmain/Point.java
+++ sandbox/src/main/java/ru/pocket/catmain/Point.java
@@ -4,4 +4,13 @@ package ru.pocket.catmain;
  * Created by Goblik on 17.08.2016.
  */
 public class Point {
+
+  public double x1;
+  public double y1;
+  public double x2;
+  public double y2;
+
 }
+
+
+
