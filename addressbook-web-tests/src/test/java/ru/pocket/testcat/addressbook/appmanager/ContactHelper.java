7636a3ca0a2406a9bc6e613ca40a580ae049c273
Anna <verdyanna@gmail.com>
2016-08-29 00:43:43 +0300

    Изменен выбор в выпадающем списке по Select. Добавлена проверка кнопки Browse

--- addressbook-web-tests/src/test/java/ru/pocket/testcat/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/pocket/testcat/addressbook/appmanager/ContactHelper.java
@@ -3,6 +3,10 @@ package ru.pocket.testcat.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.pocket.testcat.addressbook.model.ContactData;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 /**
  * Created by Goblik on 27.08.2016.
@@ -26,6 +30,7 @@ public class ContactHelper extends BaseHelper {
     type(By.name("firstname"),contactData.getFirstname());
     type(By.name("lastname"),contactData.getLastname());
     type(By.name("nickname"),contactData.getNickname());
+    wd.findElement(By.name("photo")).sendKeys("C:\\Users\\Goblik\\Pictures\\Utenok.jpg");
     type(By.name("title"),contactData.getTitle());
     type(By.name("address"),contactData.getAddress());
     type(By.name("home"),contactData.getHomePhone());
@@ -34,13 +39,13 @@ public class ContactHelper extends BaseHelper {
     type(By.name("fax"),contactData.getFax());
     type(By.name("email"),contactData.getEmail());
     type(By.name("homepage"),contactData.getHomepage());
-    click(By.xpath("//div[@id='content']/form/select[1]//option[16]"));
-    click(By.xpath("//div[@id='content']/form/select[2]//option[12]"));
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("12");
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("September");
     type(By.name("byear"),contactData.getYearbirth());
-    click(By.xpath("//div[@id='content']/form/select[3]//option[12]"));
-    click(By.xpath("//div[@id='content']/form/select[4]//option[9]"));
+    new Select(wd.findElement(By.name("aday"))).selectByVisibleText("5");
+    new Select(wd.findElement(By.name("amonth"))).selectByVisibleText("October");
     type(By.name("ayear"),contactData.getAnniver());
-    click(By.xpath("//div[@id='content']/form/select[5]//option[8]"));
+    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText("newgroup2");
     type(By.name("address2"),contactData.getAddress2());
     type(By.name("notes"),contactData.getNotes());
 
