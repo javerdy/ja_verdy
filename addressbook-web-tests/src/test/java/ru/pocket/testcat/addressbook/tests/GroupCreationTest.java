8a8bd80ab0a1b2ff94d873dd76634556616776aa
Anna <verdyanna@gmail.com>
2016-09-26 11:15:13 +0300

    9. Лямбда-выражения (анонимные функции) и элементы функционального программирования

--- addressbook-web-tests/src/test/java/ru/pocket/testcat/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/pocket/testcat/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.pocket.testcat.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,20 +14,33 @@ public class GroupCreationTest extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null,null);
+    GroupData group = new GroupData("test2", null,null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size() , before.size()+1);
 
 
-
-    int max = 0;
+//первый метод
+/*    int max = 0;
     for(GroupData g: after){
       if (g.getGroupid() >max) {
         max = g.getGroupid();
       }
-    }
-    group.setGroupid(max);
+    }*/
+
+    // второй метод
+
+
+   // int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getGroupid(), o2.getGroupid())).get().getGroupid();
+
+/*    Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+      @Override
+      public int compare(GroupData o1, GroupData o2) {
+        return Integer.compare(o1.getGroupid(), o2.getGroupid());
+      }
+    };*/
+
+    group.setGroupid(after.stream().max((o1, o2) -> Integer.compare(o1.getGroupid(), o2.getGroupid())).get().getGroupid());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
 
