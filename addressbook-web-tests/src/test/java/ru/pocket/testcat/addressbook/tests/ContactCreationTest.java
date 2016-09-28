ea8f2a8ced45e672193dce3ed452909e8594684b
Anna <verdyanna@gmail.com>
2016-09-28 18:42:59 +0300

    Ошибка contact creation

--- addressbook-web-tests/src/test/java/ru/pocket/testcat/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/pocket/testcat/addressbook/tests/ContactCreationTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 import ru.pocket.testcat.addressbook.model.ContactData;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTest extends TestBase {
