291f811b8b0e75abe8917dc9c8973786e7f6cd31
Anna <verdyanna@gmail.com>
2016-10-03 12:06:20 +0300

    6.4. Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/ru/pocket/testcat/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/pocket/testcat/addressbook/tests/GroupCreationTest.java
@@ -1,25 +1,39 @@
 package ru.pocket.testcat.addressbook.tests;
 
 import org.hamcrest.CoreMatchers;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.pocket.testcat.addressbook.model.GroupData;
 import ru.pocket.testcat.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withGroupname("test1").withGroupheader("header2").withGroupfooter("footer1")});
+    list.add(new Object[] {new GroupData().withGroupname("test2").withGroupheader("header2").withGroupfooter("footer2")});
+    list.add(new Object[] {new GroupData().withGroupname("test3").withGroupheader("header3").withGroupfooter("footer3")});
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
+   // GroupData group = new GroupData().withGroupname(name).withGroupheader(header).withGroupfooter(footer);
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withGroupname("test2");
     app.group().create(group);
     assertThat(app.group().getgroupCount(), equalTo(before.size() + 1));
     Groups after = app.group().all();
-    assertThat(after, equalTo(before
-            .withAdded(group.withGroupid(after.stream().mapToInt((g) -> g.getGroupid()).max().getAsInt()))));
+    assertThat(after, equalTo(before.withAdded(group.withGroupid(after.stream().mapToInt((g) ->g.getGroupid()).max().getAsInt()))));
   }
 
 
