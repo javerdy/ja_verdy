a4bb7bf6d87e566fb4d718918478fd2c75dc1e39
Anna <verdyanna@gmail.com>
2016-10-16 21:38:25 +0300

    поменяла токен

--- githubsample/src/test/java/ru/pack/tests/GithubTests.java
+++ githubsample/src/test/java/ru/pack/tests/GithubTests.java
@@ -11,11 +11,21 @@ import java.io.IOException;
 public class GithubTests {
 
   @Test
-  public void testCommits() throws IOException {
-    Github github = new RtGithub("74a133d4d645cd0cb857a9a818f4ee544b5b73a9");
+  public void testCommitBarantsev() throws IOException {
+    Github github = new RtGithub("e1074a3eeb968f536fae506bfe0921f8160f7fc1");
     RepoCommits commits = github.repos().get(new Coordinates.Simple("javerdy", "ja_verdy")).commits();
     for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
       System.out.println(new RepoCommit.Smart(commit).message());
     }
   }
+
+  @Test
+  public void testCommitGmail() throws IOException {
+    Github github = new RtGithub("e1074a3eeb968f536fae506bfe0921f8160f7fc1");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("javerdy", "gettingtest")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+
 }
