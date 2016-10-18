40168ae1f7b19a30db379ccd81d57d39702a0d5f
javerdy <m.osipov.a@mail.ru>
2016-10-19 00:41:44 +0300

    Add files via upload
    upload the file with upper letter

---
+++ sandbox/src/main/java/ru/pocket/catmain/Primes.java
@@ -0,0 +1,55 @@
+package ru.pocket.catmain;
+
+/**
+ * Created by Goblik on 10.09.2016.
+ */
+public class Primes {
+
+  public static boolean isPrime(int n) {
+    for (int i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+
+    }
+    return true;
+  }
+  public static boolean isPrimeFast(int n) {
+    for (int i = 2; i < n/2; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+
+    }
+    return true;
+  }
+
+  public static boolean isPrimeFast2(int n) {
+    int m = (int) Math.sqrt(n);
+    for (int i = 2; i < m; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+
+    }
+    return true;
+  }
+  public static boolean isWhilePrime(int n) {
+    int i = 2;
+    while (i < n && n % i !=0) {
+      i++;
+    }
+    return i ==n;
+  }
+  public static boolean isPrime(long n) {
+    for (long i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+
+    }
+    return true;
+  }
+
+}
+
